c3de396f150bf95b905e2e5f6ea995086e8e7ddb
Pedro Facanha <plmfacanha.dev@gmail.com>
2026-06-28 08:35:05 -0700

    feat: stop for a break and come back soon

--- Graph.js
+++ Graph.js
@@ -1,12 +1,12 @@
+function getPossibleMoves(start, end) {}
+
 function knightMoves(start, end) {
   let priorityQueue = [start];
   let head = 0;
 
   while (head < priorityQueue.length) {
     let currPos = priorityQueue[head];
-    console.log(
-      `You are at x: ${priorityQueue[0][0]} and y: ${priorityQueue[0][1]}`,
-    );
+    console.log(`You are at x: ${currPos[0]} and y: ${currPos[1]}`);
 
     let allPossibleMoves = getPossibleMoves(start, end);
 
