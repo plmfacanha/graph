5d901f6c0d7afaa79235ce2493ef6879b170f6b8
Pedro Facanha <plmfacanha.dev@gmail.com>
2026-06-28 07:57:43 -0700

    feat: just testing out inputs to start implementing the algorithm

--- Graph.js
+++ Graph.js
@@ -1,3 +1,9 @@
-function knightMoves(start, end) {}
+function knightMoves(start, end) {
+  // TODO: find the shortest path for the knight in a chessboard
 
-// TODO: find the shortest path for the knight in a chessboard
+  queue = [start];
+
+  console.log(queue);
+}
+
+knightMoves([3, 3], [4, 3]);
